c14cafe96905b801f3cb400aed8b3c63100d3777
Joao Felipe <joaofelipeazevedo28@gmail.com>
2026-09-12 15:55:45 -0400

    modified:   package.json 	modified:   src/config/logger.ts

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,22 +1,8 @@
-import pino, { type LoggerOptions } from 'pino';
+import pino from 'pino';
 
-const baseOptions: LoggerOptions = {
+const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-};
+});
 
-const options: LoggerOptions =
-  process.env.NODE_ENV !== 'production'
-    ? {
-        ...baseOptions,
-        transport: {
-          target: 'pino-pretty',
-          options: { colorize: true },
-        },
-      }
-    : baseOptions;
-
-const logger = pino(options);
-
-export {logger}
-
+export { logger };
 export default logger;
