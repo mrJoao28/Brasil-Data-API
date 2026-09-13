01baa402fef1aab97b5116c630493ca5d67b2e96
Joao Felipe <138176128+mrJoao28@users.noreply.github.com>
2026-09-13 16:52:30 -0400

    fix: pass CNPJ directly through managed providers

--- src/providers/managedCnpjProvider.ts
+++ src/providers/managedCnpjProvider.ts
@@ -1,26 +1,18 @@
-import { env } from '../config/env';
 import type { CnpjResponse } from '../schemas/cnpj.schema';
 import type { CnpjProvider } from './cnpjProvider';
 import { brasilApiCnpjProvider } from './brasilApiCnpjProvider';
 import { openCnpjProvider } from './openCnpjProvider';
 import { ProviderManager } from './providerManager';
 
-const manager = new ProviderManager<CnpjResponse>([
-  { name: brasilApiCnpjProvider.name, execute: () => brasilApiCnpjProvider.getCompanyByCnpj(currentCnpj) },
-  { name: openCnpjProvider.name, execute: () => openCnpjProvider.getCompanyByCnpj(currentCnpj) },
+const manager = new ProviderManager<CnpjResponse, string>([
+  { name: brasilApiCnpjProvider.name, execute: (cnpj) => brasilApiCnpjProvider.getCompanyByCnpj(cnpj) },
+  { name: openCnpjProvider.name, execute: (cnpj) => openCnpjProvider.getCompanyByCnpj(cnpj) },
 ]);
-
-let currentCnpj = '';
 
 export const managedCnpjProvider: CnpjProvider = {
   name: 'ProviderManager(BrasilAPI -> OpenCNPJ)',
   async getCompanyByCnpj(cnpj: string): Promise<CnpjResponse | null> {
-    currentCnpj = cnpj;
-    try {
-      return (await manager.execute()).value;
-    } finally {
-      currentCnpj = '';
-    }
+    return (await manager.execute(cnpj)).value;
   },
 };
 
