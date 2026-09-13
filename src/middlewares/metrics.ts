73aa4f64a6232124343b87f2d7d5d54c5450c0a5
Joao Felipe <138176128+mrJoao28@users.noreply.github.com>
2026-09-13 17:31:30 -0400

    feat: record endpoint metrics

--- src/middlewares/metrics.ts
+++ src/middlewares/metrics.ts
@@ -1,12 +1,13 @@
 import type { RequestHandler } from 'express';
 import { recordRequest } from '../config/metrics';
 
-export const metricsMiddleware: RequestHandler = (_req, res, next) => {
+export const metricsMiddleware: RequestHandler = (req, res, next) => {
   const startedAt = process.hrtime.bigint();
 
   res.on('finish', () => {
     const durationMs = Number(process.hrtime.bigint() - startedAt) / 1_000_000;
-    recordRequest(res.statusCode, durationMs);
+    const endpoint = `${req.method} ${req.route?.path ?? req.path}`;
+    recordRequest(res.statusCode, durationMs, endpoint);
   });
 
   next();
