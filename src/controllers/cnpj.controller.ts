5feba5725498ef0d15fe495431d1ebe44663c805
Joao Felipe <138176128+mrJoao28@users.noreply.github.com>
2026-09-13 17:43:20 -0400

    feat: expose X-Provider on CNPJ responses

--- src/controllers/cnpj.controller.ts
+++ src/controllers/cnpj.controller.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from 'express';
-import { getCompanyByCnpj } from '../services/cnpj.service';
+import { getCompanyByCnpjWithProvider } from '../services/cnpj.service';
 import { sendSuccess } from '../utils/response';
 
 export async function getCnpj(req: Request, res: Response): Promise<void> {
   const { cnpj } = req.params as unknown as { cnpj: string };
-  const company = await getCompanyByCnpj(cnpj);
+  const { company, provider } = await getCompanyByCnpjWithProvider(cnpj);
+  res.setHeader('X-Provider', provider);
   sendSuccess(res, company);
 }
